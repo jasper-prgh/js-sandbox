a1a5ca1fcc1843d2f33ef99247f84cbb6030f459
Jasper Lichte <jasper.lichte@prgh-group.com>
2023-01-24 10:31:39 +0100

    Add naming file

---
+++ naming.js
@@ -0,0 +1,11 @@
+// camelCase
+let sumOfTwoDifferentCoolNumbers
+
+// snake_case
+let sum_of_two_different_cool_numbers
+
+// snail-case
+// sum-of-two-different-cool-numbers
+
+const CONSTANT = 8;
+CONSTANT = 3;
