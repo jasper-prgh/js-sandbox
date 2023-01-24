da76f87cb22e6e636b420c54a15a0ded1b1ea74b
Jasper Lichte <jasper.lichte@prgh-group.com>
2023-01-24 08:47:21 +0100

    add variables.js

---
+++ variables.js
@@ -0,0 +1,13 @@
+let a = 8;
+let b = 5;
+let c = a + b;
+let d = c * 6;
+
+let e = 14;
+console.log(e)
+e++; // e = e + 1;
+e += 2; // e = e + 2
+console.log(e)
+e--; // e = e - 1;
+e -= 2; // e = e - 2
+console.log(e)
