b4bdab6cdf7762915a36e1f291cb2109043fa3af
Jasper Lichte <jasper.lichte@prgh-group.com>
2023-01-25 08:32:52 +0100

    Add switch cases

--- controlStructures.js
+++ controlStructures.js
@@ -13,4 +13,22 @@ if (operation == '+') {
     result = num1 / num2;
 }
 
+switch (operation) {
+    case '+':
+        result = num1 + num2;
+        break;
+    case '-':
+        result = num1 - num2;
+        break;
+    case '*':
+        result = num1 * num2;
+        break;
+    case '/':
+        result = num1 / num2;
+        break;
+}
+
+
+// if, for, while, do-while
+
 console.log(num1 + operation + num2 + '=' + result);
