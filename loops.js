e99c58c8ddb7be7d80ce13b852f09bd60737a001
Jasper Lichte <jasper.lichte@prgh-group.com>
2023-01-26 09:51:38 +0100

    Add loops

--- loops.js
+++ loops.js
@@ -1,10 +1,14 @@
-let i = 0;
-while (i < 20) {
-    console.log(i);
+// let i = 0;
+// while (i < 20) {
+//     console.log(i);
 
-    i++;
-}
+//     i++;
+// }
 
 for (let j = 0; j < 20; j++) {
+    if (j == 5) {
+        continue;
+    }
+
     console.log(j);
 }
